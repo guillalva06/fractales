c6c7857f93f43462f17d6270afec7b3b7c30c778
Guillermo Alvarez <guillermo.alvarez@mail.escuelaing.edu.co>
2016-06-21 23:16:08 -0500

    Chaos Game y Koch Curve advance

---
+++ game.js
@@ -0,0 +1,42 @@
+window.onload = init;
+
+function init(){
+	var canvas = document.getElementById('mycanvas');
+	canvas.width = 500;
+	canvas.height = 500;
+	var r = 240;
+	var center = new Point(250,250);
+	var ctx = canvas.getContext('2d');
+	var n = 6;
+	var points = [];
+	for(var i=0;i<n;i++){
+		var x = center.x + r*Math.cos(i*2*Math.PI/n);
+		var y = center.y + r*Math.sin(i*2*Math.PI/n);
+		points.push(new Point(x,y));
+		drawpoint(points[i]);
+	}	
+	ctx.beginPath();
+	ctx.arc(center.x,center.y,r,0,2*Math.PI);
+	ctx.stroke();
+	var r = 1;
+	var iterations = 30000;
+	var point = new Point(250,250);
+	drawpoint(point);
+	for(var i=0;i<iterations;i++){
+		var diece = Math.floor(Math.random()*n);
+		point.x = (points[diece].x+point.x*r)/(1+r)+0*point.x;
+		point.y = (points[diece].y+point.y*r)/(1+r)+0*point.y;
+		drawpoint(point);
+	}
+}
+
+function drawpoint(point){
+	var canvas = document.getElementById('mycanvas');
+	var ctx = canvas.getContext('2d');
+	ctx.fillRect(point.x,point.y,1,1);
+}
+
+function Point(x,y){
+	this.x = x;
+	this.y = y;
+}
--- koch.js
+++ koch.js
@@ -3,14 +3,16 @@ window.onload = init;
 function init(){
 	//console.log('Hola Mundo JS');
 	var canvas = document.getElementById('mycanvas');
-	canvas.width = 500;
-	canvas.height = 500;
+	canvas.width = 2500;
+	canvas.height = 2500;
 	var ctx = canvas.getContext('2d');
-	var linea = new line(100,200,300,0);
-	printline(linea,'black');
+	ctx.lineWidth = 1;
+	var linea = new line(100,200,1000,Math.PI/4*0,0);
+	//var linea = new line(100,200,300,0);
+	printline(linea,'blue');
 	var lineas = [];
 	lineas.push(linea);
-	for(var i=0;i<2;i++){
+	for(var i=0;i<5;i++){
 		var newlines = [];
 		for(x in lineas){
 			console.log(lineas[x]);
@@ -18,35 +20,41 @@ function init(){
 		}
 		lineas = newlines;
 	}
-	generateLines(linea,lineas);
 	//console.log(ctx);
 };
 
 
-function line(x,y,l,theta){
+function line(x,y,l,theta,type){
 	this.x = x;
 	this.y = y;
 	this.l = l;
 	this.theta = theta;
-	//console.log(this);
+	this.type = type;
 };
 
 function generateLines(linea,lineas){
 	var x = linea.x;
 	var y = linea.y;
 	var l = linea.l/3;
+	var sen = Math.sin(linea.theta);
+	var cos = Math.cos(linea.theta);
 	var theta = linea.theta;
-	var line1 = new line(x,y,l,theta);
-	var line2 = new line(x+l,y,l,theta-Math.PI/3);
-	var line3 = new line(x+2*l,y,l,theta-2*Math.PI/3);
-	var line4 = new line(x+2*l,y,l,theta);
+	var line1 = new line(x,y,l,theta,linea.type);
+	var line4 = new line(x+2*l*cos,y+2*l*sen,l,theta,linea.type);
+	if(linea.type%2 == 0){
+		var line2 = new line(x+l*cos,y+l*sen,l,theta-Math.PI/3,linea.type);
+		var line3 = new line(x+2*l*cos,y+2*l*sen,l,theta-2*Math.PI/3,linea.type+1);
+	}else{
+		var line2 = new line(x+l*cos,y+l*sen,l,theta+Math.PI/3,linea.type);
+		var line3 = new line(x+2*l*cos,y+2*l*sen,l,theta+2*Math.PI/3,linea.type+1);
+	}
 	lineas.push(line1);
 	lineas.push(line2);
 	lineas.push(line3);
 	lineas.push(line4);
-	printline(line2,'green');
+	printline(line2,'blue');
 	printline(line3,'blue');
-	printline(new line(x+l,y,l,theta),'white');
+	eraseline(new line(x+l*cos,y+l*sen,l,theta));
 	return null;
 };
 
@@ -63,5 +71,14 @@ function printline(line,color){
 	ctx.lineTo(line.l,0);
 	ctx.stroke();
 	ctx.restore();
-	//console.log('End print line');
+}
+
+function eraseline(line){
+	var canvas = document.getElementById('mycanvas');
+	var ctx = canvas.getContext('2d');
+	ctx.save();
+	ctx.translate(line.x,line.y);
+	ctx.rotate(line.theta);
+	ctx.clearRect(1,-1,line.l-2,2);
+	ctx.restore();	
 }
