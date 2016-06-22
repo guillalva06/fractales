4a44e6538f15fe13bacdc13cfbd3da855660781f
Guillermo Alvarez <guillermo.alvarez@mail.escuelaing.edu.co>
2016-06-22 07:18:32 -0500

    Change on distance on chaos game

--- game.js
+++ game.js
@@ -7,7 +7,7 @@ function init(){
 	var r = 240;
 	var center = new Point(250,250);
 	var ctx = canvas.getContext('2d');
-	var n = 6;
+	var n = 5;
 	var points = [];
 	for(var i=0;i<n;i++){
 		var x = center.x + r*Math.cos(i*2*Math.PI/n);
@@ -18,14 +18,16 @@ function init(){
 	ctx.beginPath();
 	ctx.arc(center.x,center.y,r,0,2*Math.PI);
 	ctx.stroke();
-	var r = 1;
+	var r = 1-1/2;
 	var iterations = 30000;
 	var point = new Point(250,250);
 	drawpoint(point);
 	for(var i=0;i<iterations;i++){
 		var diece = Math.floor(Math.random()*n);
-		point.x = (points[diece].x+point.x*r)/(1+r)+0*point.x;
-		point.y = (points[diece].y+point.y*r)/(1+r)+0*point.y;
+		//point.x = (points[diece].x+point.x*r)/(1+r)+0*point.x;
+		//point.y = (points[diece].y+point.y*r)/(1+r)+0*point.y;
+		point.x = (points[diece].x-point.x)*r+point.x;
+		point.y = (points[diece].y-point.y)*r+point.y;		
 		drawpoint(point);
 	}
 }
